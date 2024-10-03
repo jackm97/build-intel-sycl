1e8c5d4efbfae4234b4b27368eaf8e261f15ed19
Jack Myers <jack.myers@intel.com>
2024-10-03 05:49:59 +0000

    fixes
    Signed-off-by: Jack Myers <jack.myers@intel.com>

---
+++ scripts/scripts/configure.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -x
+
+extra_opts=""
+for var in $CMAKE_ARGS; do
+  if [[ ! "$var" == *"CMAKE_BUILD_TYPE"* ]]; then
+    extra_opts="--cmake-opt=$var $extra_opts"
+  fi
+done
+
+python "$DPCPP_HOME"/llvm/buildbot/configure.py \
+  --cuda --native_cpu \
+  --llvm-external-projects="clang-tools-extra,compiler-rt" \
+  --cmake-opt=-DCUDA_TOOLKIT_ROOT_DIR="$CONDA_PREFIX"/targets/x86_64-linux/ \
+  --cmake-opt=-DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH" \
+  --cmake-opt=-DCMAKE_C_COMPILER_LAUNCHER=ccache \
+  --cmake-opt=-DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+  --cmake-opt=-DCMAKE_INSTALL_PREFIX="$ONEAPI_INSTALL_DIR" \
+  --cmake-opt="-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=SPIRV" \
+  --cmake-opt="-DLLVM_INSTALL_UTILS=ON" \
+  "$extra_opts"
