9dc404b9ed65fd860c754bde17011ccb9882eccf
Jack Myers <jackhyers97@gmail.com>
2024-11-25 13:45:49 -0800

    WIP
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- recipes/llvm-sycl/files/pixi-activate.sh
+++ recipes/llvm-sycl/files/pixi-activate.sh
@@ -3,10 +3,10 @@
 set -e
 
 oneapi_dir=$(bash -c "cd -- $(dirname -- "${BASH_SOURCE[0]}") &>/dev/null && pwd")
-cp -r "$oneapi_dir/*" "$BUILD_PREFIX"
-export CXXFLAGS="--sysroot=$CONDA_BUILD_SYSROOT --gcc-toolchain=$BUILD_PREFIX --target=$HOST $CXXFLAGS"
-export CFLAGS="--sysroot=$CONDA_BUILD_SYSROOT --gcc-toolchain=$BUILD_PREFIX --target=$HOST $CFLAGS"
-export LDFLAGS="-Wl,rpath,$BUILD_PREFIX/lib64 -L$BUILD_PREFIX/lib64 $LDFLAGS"
+find "$oneapi_dir" -maxdepth 1 -exec cp -r {} "$BUILD_PREFIX" ";"
+export CXXFLAGS="--sysroot=$CONDA_BUILD_SYSROOT --gcc-toolchain=$BUILD_PREFIX --target=$CONDA_TOOLCHAIN_HOST $CXXFLAGS"
+export CFLAGS="--sysroot=$CONDA_BUILD_SYSROOT --gcc-toolchain=$BUILD_PREFIX --target=$CONDA_TOOLCHAIN_HOST $CFLAGS"
+export LDFLAGS="-Wl,-rpath,$BUILD_PREFIX/lib64 -L$BUILD_PREFIX/lib64 $LDFLAGS"
 echo "$CXXFLAGS" >"$oneapi_dir/bin/clang++.cfg"
 echo "$LDFLAGS" >>"$oneapi_dir/bin/clang++.cfg"
 echo "$CFLAGS" >"$oneapi_dir/bin/clang.cfg"
